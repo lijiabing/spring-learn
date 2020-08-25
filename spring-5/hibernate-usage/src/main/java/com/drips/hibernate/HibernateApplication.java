151607b8a3bb2a77c6d424548eb0f0656bb0ffb6
lijiabing <jackbing228@gmail.com>
2020-08-25 17:54:41 +0800

    --

---
+++ spring-5/hibernate-usage/src/main/java/com/drips/hibernate/HibernateApplication.java
@@ -0,0 +1,15 @@
+package com.drips.hibernate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author lijb
+ */
+@SpringBootApplication
+public class HibernateApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HibernateApplication.class, args);
+    }
+}
---
+++ spring-5/hibernate-usage/src/main/java/com/drips/hibernate/entity/Singer.java
@@ -0,0 +1,48 @@
+package com.drips.hibernate.entity;
+
+import javax.persistence.*;
+import java.io.Serializable;
+
+/**
+ * @author lijb
+ */
+@Table(name = "singer")
+@Entity
+public class Singer implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "ID")
+    private Long id;
+
+    @Column(name = "NAME")
+    private String name;
+
+    @Version
+    @Column(name = "VERSION")
+    private Integer version;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getVersion() {
+        return version;
+    }
+
+    public void setVersion(Integer version) {
+        this.version = version;
+    }
+}
